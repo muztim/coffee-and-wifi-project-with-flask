3837e4554c9965119386ecb610ae52a77afdab8a
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2022-11-09 15:14:53 +0300

    add: -cafes.html page -add CafeForm class -update modules to requirements -add 'add' route to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
+from cafe_form import CafeFrom
+import csv
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -9,6 +10,14 @@ Bootstrap(app)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/add')
+def add_cafe():
+    form = CafeFrom()
+    if form.validate_on_submit():
+        print("True")
+    return render_template('add.html', form=form)
 
 
 if __name__ == "__main__":
